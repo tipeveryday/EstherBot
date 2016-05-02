2fbcc4d3c6cf8ce61172de4e70c14bddb433a92d
Mike Mason <masonmike252@gmail.com>
2016-05-02 19:53:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,6 @@ module.exports = new Script({
         //prompt: (bot) => bot.say('Beep boop...'),
         receive: () => 'processing'
     },
-
-    start: {
-        receive: (bot) => {
-            return bot.say('Hi friend. I am an experiment to download a little bit of Mike Mason into a bot you can have a conversation with.\nI learn as I talk to more people, so I cherishg the opportunity to speak with you.\nIf you want to proceed type "begin" ')
-                .then(() => 'speak');
-        }
-    },
     
     start: {
         receive: (bot) => {
